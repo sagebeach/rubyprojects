b148e2ba20e042c1caa66dafb15632e33d1ab865
Sagebeach <spicywiseman@gmail.com>
2018-03-15 10:56:02 -0700

    Shortest and longest paths
    Includes implementation of Dijkstra's algorithm to find the shortest path through the maze and the longest path.

--- mazes/dijkstra.rb
+++ mazes/dijkstra.rb
@@ -1,11 +1,15 @@
 require 'distance_grid'
-require 'binary_tree'
+require 'binary_tree' #replace with algorithm filename: 'binary_tree', 'sidewinder',
 
 grid = DistanceGrid.new(5, 5)
-BinaryTree.on(grid)
+BinaryTree.on(grid) #call the class associated with the algorithm specified above: "BinaryTree", "Sidewinder",
 
 start = grid[0, 0]
 distances = start.distances
 
 grid.distances = distances
 puts grid
+
+puts "path from northwest corner to southwest corner:"
+grid.distances = distances.path_to(grid[grid.rows - 1, 0])
+puts grid.to_s
