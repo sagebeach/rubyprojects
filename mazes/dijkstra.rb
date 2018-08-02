4f96e4ea164270edf8dd34e0e84596091e07f10a
Sage Beach <32965757+sagebeach@users.noreply.github.com>
2018-08-02 04:09:15 -0700

    Revert "Shortest and longest paths"
    This reverts commit b148e2ba20e042c1caa66dafb15632e33d1ab865.

--- mazes/dijkstra.rb
+++ mazes/dijkstra.rb
@@ -1,15 +1,11 @@
 require 'distance_grid'
-require 'binary_tree' #replace with algorithm filename: 'binary_tree', 'sidewinder',
+require 'binary_tree'
 
 grid = DistanceGrid.new(5, 5)
-BinaryTree.on(grid) #call the class associated with the algorithm specified above: "BinaryTree", "Sidewinder",
+BinaryTree.on(grid)
 
 start = grid[0, 0]
 distances = start.distances
 
 grid.distances = distances
 puts grid
-
-puts "path from northwest corner to southwest corner:"
-grid.distances = distances.path_to(grid[grid.rows - 1, 0])
-puts grid.to_s
